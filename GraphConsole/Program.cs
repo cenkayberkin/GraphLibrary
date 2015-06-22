b2d16a5ca13181149641a6f0e422cd05b94b43ec
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-06-22 20:58:49 +0300

    Degree , maxdegree added and List changed to hashset

--- GraphConsole/Program.cs
+++ GraphConsole/Program.cs
@@ -8,6 +8,11 @@ namespace GraphConsole
 		public static void Main (string[] args)
 		{
 			Graph g = new Graph ("../../../GraphLibrary/tinyGraph.txt");
+			g.AddEdge (0, 6);
+			g.AddEdge (0, 6);
+
+			Console.WriteLine (g.MaxDegree());
+
 			Console.WriteLine (g.ToString ());
 
 			Console.WriteLine ("Hello World!");
